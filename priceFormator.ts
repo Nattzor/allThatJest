c2ca5e2d473918c87cbb1c417272ca358d5b3bc8
Markus <markus.mork@iths.se>
2022-12-02 10:21:03 +0100

    Fix currency bugg

--- priceFormator.ts
+++ priceFormator.ts
@@ -1,25 +1,9 @@
 const priceFormator = (price:number, currency?:string)=> {
-    
+
     if(currency?.includes('%PRICE%')){
-        console.log(currency.replace('%PRICE%',price.toFixed(2).toString()))
         return currency.replace('%PRICE%',price.toFixed(2).toString())
-        
-       
         } else{
-
-           return price.toFixed(2).toString() + ' ' + currency?currency:'SEK'
-
+           return price.toFixed(2).toString() + ' ' + (currency ? currency:'SEK')
         }
-
-        
-    
 }
-
-// return currency?.replace('%PRICE%',price.toString())  
-
-
 export default priceFormator
-
-// roundPrice(232.10542, '%PRICE% kr') → "232.11 kr"
-// roundPrice(14, '$%PRICE%') → "$14.00"
-// roundPrice(1024.2048, 'USD %PRICE%') → "USD 1024.20 SEK"
