8024789139e88158ca4ab0aff4cdc5e3569b1cfb
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-12-05 10:34:47 +0100

    övning 9

--- functions.ts
+++ functions.ts
@@ -9,16 +9,7 @@ export const formatName = (name: string): string => {
    return name.endsWith('s')? name : name+'s'
 }
 
-export type Arr = {
-    name: string
-    group: Number
-}
-
-export const getUsers = (arr: Arr) => {
- console.log("testar getUsers",getUsers)
-}
-
-const arr = [
+const arr: Arr[] = [
     {
         "name": "Erik",
         "group": 1
@@ -44,3 +35,17 @@ const arr = [
         "group": 3
     }
  ]
+
+const result: Arr[] = await Promise.all(arr.map(async (elem): Promise<Arr> => {
+    await getUsers();
+    return elem;
+}));
+
+export type Arr = {
+    name: string
+    group: Number
+}
+
+export const getUsers = (arr: Arr) => {
+ console.log("testar getUsers",getUsers)
+}
